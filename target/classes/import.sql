5be4214e836759dce4d4e7df89c1c1f07f4bb587
Fernando <i201511184@cibertec.edu.pe>
2021-08-28 13:58:47 -0500

    Adding .gitignore file

---
+++ target/classes/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_tipo_cambio (monto, nuevo_monto, moneda_origen, moneda_destino, tipo_cambio, fecha_creacion) VALUES (150.5, 602, 'PE', 'USD', 4.09, NOW());
+INSERT INTO tb_tipo_cambio (monto, nuevo_monto, moneda_origen, moneda_destino, tipo_cambio, fecha_creacion) VALUES (200, 820, 'PE', 'USD', 4.10, NOW());
+INSERT INTO tb_tipo_cambio (monto, nuevo_monto, moneda_origen, moneda_destino, tipo_cambio, fecha_creacion) VALUES (256, 1036.8, 'PE', 'USD', 4.05, NOW());
